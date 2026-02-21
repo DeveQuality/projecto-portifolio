0faa0d403cc1d4b5420f5cc82fa7769ac0fb1c71
Flora <fmacuacua116@gmail.com>
2026-02-20 20:40:16 -0800

    de novo

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 alert('Seja bem-vindo ao meu portifólio')
 const nome=prompt('Qual o seu nome ?')
-alert('Ola ' + nome + '! Esteja a vontade.')
+alert('Ola ' + nome + '! Esteja a vontade para explorar o meu portifólio.')
 
 const botao = document.getElementById('botao-tema');
 const body = document.body;
